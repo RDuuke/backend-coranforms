2f4e4add0090cc48e910f997aae32e3b38179580
daesca <reickchozo@gmail.com>
2023-07-20 19:19:37 -0500

    Feature/evaluaciones/delete antecedentes (#3)
    * Agregando endpoint de Borrar Situacion Encontrada
    
    * Iniciando endpoint de borrar antecedentes
    
    * Endpoint Borrar Antecedente

---
+++ src/main/java/co/gov/coran/licencias/controller/BorrarAntecedenteController.java
@@ -0,0 +1,22 @@
+package co.gov.coran.licencias.controller;
+
+import co.gov.coran.licencias.models.dto.BorrarAntecedenteDTO;
+import co.gov.coran.licencias.service.BorrarAntecedenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BorrarAntecedenteController {
+
+    @Autowired
+    private BorrarAntecedenteService borrarAntecedenteService;
+
+    @PostMapping("/borrar_antecedente")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+
+    public String borrarAntecedente(@RequestBody BorrarAntecedenteDTO borrarAntecedenteDTO){
+        return this.borrarAntecedenteService.borrarAntecedente(borrarAntecedenteDTO);
+    }
+}
