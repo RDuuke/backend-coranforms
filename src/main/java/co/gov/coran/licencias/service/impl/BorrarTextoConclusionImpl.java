ff84ec4c09654e9d588501522919d3a837db9581
daesca <reickchozo@gmail.com>
2023-07-26 12:00:59 -0500

    Resolviendo error de servicio (#11)

--- src/main/java/co/gov/coran/licencias/service/impl/BorrarTextoConclusionImpl.java
+++ src/main/java/co/gov/coran/licencias/service/impl/BorrarTextoConclusionImpl.java
@@ -4,7 +4,9 @@ import co.gov.coran.licencias.models.dto.BorrarTextoConclusionDTO;
 import co.gov.coran.licencias.repository.BorrarTextoConclusionRepository;
 import co.gov.coran.licencias.service.BorrarTextoConclusionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BorrarTextoConclusionImpl implements BorrarTextoConclusionService {
 
     @Autowired
