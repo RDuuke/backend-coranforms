64c6d28f739bf7393e74c50c6b9bd822a06a19c0
daesca <reickchozo@gmail.com>
2023-08-28 09:36:24 -0500

    Feature/evaluaciones/endpoint guardar nuevo agente peligro (#25)
    * Endpoint de consulta agentes peligrosos
    
    * Añadiendo nuevos campos al endpoint
    
    Campo ciJustificacionAgentes
    Campo tiAgentesPeligro
    
    * Funcionalidad de guardar nuevo agente peligro

---
+++ src/main/java/co/gov/coran/licencias/service/GuardarNuevoAgentePeligroService.java
@@ -0,0 +1,9 @@
+package co.gov.coran.licencias.service;
+
+import co.gov.coran.licencias.models.dto.GuardarNuevoAgentePeligroDTO;
+
+public interface GuardarNuevoAgentePeligroService {
+
+    GuardarNuevoAgentePeligroDTO guardarNuevoAgentePeligro(GuardarNuevoAgentePeligroDTO nuevoAgentePeligroDTO);
+
+}
