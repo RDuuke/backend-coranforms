fda741a777aa1929c15b1072d5a01022c607faae
jhonMontano <115125752+jhonMontano@users.noreply.github.com>
2023-09-13 10:41:20 -0500

    Feature/ajustar endpoint guardar coordenadas (#37)
    * Se crea el entpoint de borrar coordenada
    
    * Actualización endpoint borrar_coordenadas
    
    * Se ajusta el endpoint guardar coordenadas para que se puedan guardar varias
    
    ---------
    
    Co-authored-by: practicante_planea1 <practicante_planea1@corantioquia.gov.co>
    Co-authored-by: Juan Duque <juuanduuke@gmail.com>

--- src/main/java/co/gov/coran/licencias/licenciaConfig/WebSecurityConfig.java
+++ src/main/java/co/gov/coran/licencias/licenciaConfig/WebSecurityConfig.java
@@ -66,8 +66,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 "/consulta_general","/consulta_todas_aspect", "/borrar_obligacion", "/borrar_situacion_obligacion",
                 "/borrar_antecedente", "/borrar_texto_recomenda", "/borrar_texto_conclusion", "/borrar_analisis_info",
                 "/borrar_antecedente_gnral", "/consultar_tipos_agente_peligro", "/borrar_coordenadas",
-                "/guardar_nuevo_agente_peligro", "/consulta_afectacion_ambiental"
-
+                "/guardar_nuevo_agente_peligro", "/consulta_afectacion_ambiental", "/guardar_coordenadas"
         );
     }
     @Autowired
--- src/main/java/co/gov/coran/licencias/service/impl/GuardarCoordenadasServiceImpl.java
+++ src/main/java/co/gov/coran/licencias/service/impl/GuardarCoordenadasServiceImpl.java
@@ -3,17 +3,21 @@ package co.gov.coran.licencias.service.impl;
 import co.gov.coran.licencias.models.dto.GuardarCoordenadasDTO;
 import co.gov.coran.licencias.repository.GuardarCoordenadasRepository;
 import co.gov.coran.licencias.service.GuardarCoordenadasService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 
 @Service
 public class GuardarCoordenadasServiceImpl implements GuardarCoordenadasService {
 
-    @Autowired
-    private GuardarCoordenadasRepository guardarCoordenadasRepository;
+    private final GuardarCoordenadasRepository guardarCoordenadasRepository;
+
+    public GuardarCoordenadasServiceImpl(GuardarCoordenadasRepository guardarCoordenadasRepository) {
+        this.guardarCoordenadasRepository = guardarCoordenadasRepository;
+    }
+
     @Override
-    public GuardarCoordenadasDTO guardarCoord(GuardarCoordenadasDTO guardarCoordenadasDTO) {
-        return guardarCoordenadasRepository.guardarCoord(guardarCoordenadasDTO);
+    public List<GuardarCoordenadasDTO> guardarCoordenadas(List<GuardarCoordenadasDTO> guardarCoordenadasDTOList) {
+        return guardarCoordenadasRepository.guardarCoord(guardarCoordenadasDTOList);
     }
 }
