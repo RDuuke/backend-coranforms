aef594810b935370a1ec844c5ec462661f54be98
Juan Duque <juuanduuke@gmail.com>
2023-09-18 18:09:44 -0500

    Add Services e Implementaciones

--- src/main/java/co/gov/coran/licencias/service/ProblematicaConsultaByLineaService.java
+++ src/main/java/co/gov/coran/licencias/service/ProblematicaConsultaByLineaService.java
@@ -1,2 +1,11 @@
-package co.gov.coran.licencias.service;public interface ProblematicaConsultaByLinea {
+package co.gov.coran.licencias.service;
+
+import co.gov.coran.licencias.models.dto.ProblematicaGuardarDTO;
+
+import java.math.BigDecimal;
+
+public interface ProblematicaConsultaByLineaService {
+
+    ProblematicaGuardarDTO problematicaItem(BigDecimal niSecEEta, BigDecimal niLinea);
+
 }
