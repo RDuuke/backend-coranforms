7f596a62e9bcc3f740e6ecfa34cc6290f8a79a76
jhonMontano <115125752+jhonMontano@users.noreply.github.com>
2023-09-18 18:19:20 -0500

    Feature/borrar texto conclusion (#39)
    * Se ajustó endpoint borrar_texto_recomenda
    
    * Se ajusta endpoint borrar_texto_conclusion
    
    ---------
    
    Co-authored-by: practicante_planea1 <practicante_planea1@corantioquia.gov.co>

--- src/main/java/co/gov/coran/licencias/repository/BorrarTextoConclusionRepository.java
+++ src/main/java/co/gov/coran/licencias/repository/BorrarTextoConclusionRepository.java
@@ -2,7 +2,6 @@ package co.gov.coran.licencias.repository;
 
 import co.gov.coran.licencias.models.dto.BorrarTextoConclusionDTO;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
 import javax.persistence.ParameterMode;
 import javax.persistence.StoredProcedureQuery;
@@ -12,7 +11,7 @@ import java.math.BigDecimal;
 public class BorrarTextoConclusionRepository {
 
     private final EntityManager entityManager;
-    private static  final String storeProcedureName="sirena.pks_it_gnral.borrar_texto_conclusion";
+    private static  final String BORRAR_TEXTO_CONNCLUSION = "sirena.pks_it_gnral.borrar_texto_conclusion";
 
     BorrarTextoConclusionRepository(EntityManager entityManager){
         this.entityManager = entityManager;
@@ -20,14 +19,14 @@ public class BorrarTextoConclusionRepository {
 
     public String borrarTextoConclusion(BorrarTextoConclusionDTO borrarTextoConclusionDTO){
 
-        StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery(storeProcedureName);
+        StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery(BORRAR_TEXTO_CONNCLUSION);
         storedProcedureQuery.registerStoredProcedureParameter("niSecEEta", BigDecimal.class, ParameterMode.IN);
-        storedProcedureQuery.registerStoredProcedureParameter("niLinea", BigDecimal.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("nioLinea", BigDecimal.class, ParameterMode.IN);
         storedProcedureQuery.registerStoredProcedureParameter("viIdUsuario", String.class, ParameterMode.IN);
         storedProcedureQuery.registerStoredProcedureParameter("voError", String.class, ParameterMode.OUT);
 
-        storedProcedureQuery.setParameter("niSecEEta", borrarTextoConclusionDTO.getNiSec());
-        storedProcedureQuery.setParameter("niLinea", borrarTextoConclusionDTO.getNioLinea());
+        storedProcedureQuery.setParameter("niSecEEta", borrarTextoConclusionDTO.getNiSecEEta());
+        storedProcedureQuery.setParameter("nioLinea", borrarTextoConclusionDTO.getNioLinea());
         storedProcedureQuery.setParameter("viIdUsuario", borrarTextoConclusionDTO.getViIdUsuario());
 
 
