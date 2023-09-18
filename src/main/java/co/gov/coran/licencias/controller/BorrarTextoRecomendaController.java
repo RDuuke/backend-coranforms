9b4229c0a06451ef84dccda875d13f4de8fe0d09
jhonMontano <115125752+jhonMontano@users.noreply.github.com>
2023-09-18 18:18:52 -0500

    Se ajustó endpoint borrar_texto_recomenda (#38)
    Co-authored-by: practicante_planea1 <practicante_planea1@corantioquia.gov.co>

--- src/main/java/co/gov/coran/licencias/controller/BorrarTextoRecomendaController.java
+++ src/main/java/co/gov/coran/licencias/controller/BorrarTextoRecomendaController.java
@@ -13,8 +13,8 @@ public class BorrarTextoRecomendaController {
 
     @PostMapping("/borrar_texto_recomenda")
     @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public String borrarTextoRecomenda(@RequestBody BorrarTextoRecomendaDTO borrarTextoRecomendaDTO){
+    public @ResponseBody
+    String borrarTextoRecomenda(@RequestBody BorrarTextoRecomendaDTO borrarTextoRecomendaDTO){
         return this.borrarTextoRecomendaService.borrarTextoRecomenda(borrarTextoRecomendaDTO);
     }
 }
