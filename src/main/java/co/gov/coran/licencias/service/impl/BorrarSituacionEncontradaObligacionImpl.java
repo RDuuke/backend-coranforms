a093b38d06889cd6e633fb776f923e546402c22d
practicante_planea1 <practicante_planea1@corantioquia.gov.co>
2023-10-10 09:02:11 -0500

    Se ajusta este endpoint ya que tenia conflictos al borrar una situación encontrada

--- src/main/java/co/gov/coran/licencias/service/impl/BorrarSituacionEncontradaObligacionImpl.java
+++ src/main/java/co/gov/coran/licencias/service/impl/BorrarSituacionEncontradaObligacionImpl.java
@@ -4,14 +4,15 @@ import co.gov.coran.licencias.models.dto.BorrarSituacionEncontradaObligacionDTO;
 import co.gov.coran.licencias.repository.BorrarSituacionEncontradaObligacionRepository;
 import co.gov.coran.licencias.service.BorrarSituacionEncontradaObligacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BorrarSituacionEncontradaObligacionImpl implements BorrarSituacionEncontradaObligacionService {
 
     @Autowired
     private BorrarSituacionEncontradaObligacionRepository borrarObligacionRepository;
 
-    public String borrarSituacionEncontradaObligacion(BorrarSituacionEncontradaObligacionDTO borrarSituacionEncontradaObligacionDTO){
-
+    public String borrarSituacionEncontrada(BorrarSituacionEncontradaObligacionDTO borrarSituacionEncontradaObligacionDTO){
         return this.borrarObligacionRepository.borrarSituacionEncontradaObligacion(borrarSituacionEncontradaObligacionDTO);
 
     }
