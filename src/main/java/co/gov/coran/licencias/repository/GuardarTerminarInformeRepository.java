91fa09477f62307788a8a4ced57736139633a7fe
jhonMontano <115125752+jhonMontano@users.noreply.github.com>
2023-10-11 08:25:30 -0500

    Se crea el endpoint terminar_informe (#46)
    Co-authored-by: practicante_planea1 <practicante_planea1@corantioquia.gov.co>

---
+++ src/main/java/co/gov/coran/licencias/repository/GuardarTerminarInformeRepository.java
@@ -0,0 +1,36 @@
+package co.gov.coran.licencias.repository;
+
+import co.gov.coran.licencias.models.dto.GuardarTerminarInformeDTO;
+import org.springframework.stereotype.Repository;
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
+import java.math.BigDecimal;
+@Repository
+public class GuardarTerminarInformeRepository {
+
+    private final EntityManager entityManager;
+    private static final String TERMINAR_INFORME = "sirena.pks_it_gnral.terminar_informe";
+    public GuardarTerminarInformeRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public GuardarTerminarInformeDTO guardarTerminarInforme(GuardarTerminarInformeDTO guardarTerminarInformeDTO){
+        String voerror = null;
+        StoredProcedureQuery storedProcedureQuery = entityManager.createStoredProcedureQuery(TERMINAR_INFORME);
+        storedProcedureQuery.registerStoredProcedureParameter("niSecEEta" , BigDecimal.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("viIdUsuario", String.class, ParameterMode.IN);
+        storedProcedureQuery.registerStoredProcedureParameter("voError", String.class, ParameterMode.OUT);
+
+        storedProcedureQuery.setParameter("niSecEEta", guardarTerminarInformeDTO.getNiSecEEta());
+        storedProcedureQuery.setParameter("viIdUsuario", guardarTerminarInformeDTO.getViIdUsuario());
+
+        storedProcedureQuery.execute();
+
+        Object voError = storedProcedureQuery.getOutputParameterValue("voError");
+
+        guardarTerminarInformeDTO.setVoError(String.valueOf(storedProcedureQuery.getOutputParameterValue("voError")));
+
+        return guardarTerminarInformeDTO;
+    }
+}
---
+++ src/main/java/co/gov/coran/licencias/service/GuardarTerminarInformeService.java
@@ -0,0 +1,7 @@
+package co.gov.coran.licencias.service;
+
+import co.gov.coran.licencias.models.dto.GuardarTerminarInformeDTO;
+
+public interface GuardarTerminarInformeService {
+     GuardarTerminarInformeDTO guardarTerInforme (GuardarTerminarInformeDTO guardarTerminarInformeDTO);
+}
